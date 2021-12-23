7ad058df607da8b52ce4bcc0900857a477daae56
bassamejaz <96580385+bassamejaz@users.noreply.github.com>
2021-12-23 18:16:01 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleDelete = (counterId) => {
+    console.log("handle delete event called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    console.log("reset");
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    console.log("inside handleIncrement");
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+    console.log("in handle decrement");
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            onDecrement={this.handleDecrement}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
